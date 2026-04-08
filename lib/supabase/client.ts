e7e6169dd53d5e1489c73d1a2d4e07d6d457d51c
onelx <garciaosvaldo@gmail.com>
2026-04-08 16:17:56 -0300

    feat: add lib/supabase/client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createBrowserClient } from "@supabase/ssr";
 
-// Cliente de Supabase para el navegador
+// Cliente de Supabase para el lado del cliente (browser)
 export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  );
 }
